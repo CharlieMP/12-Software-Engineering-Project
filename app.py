af0e2310c07a8b13dee4cb8b5ce8d4068b2b9a87
Your Namepuclear <you@example.com>
2026-08-18 20:53:15 +1000

    began to write the pseudocode for one of the modules and began coding the main line of the program

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template, redirect, url_for, send_from_directory
+from sqlalchemy import create_engine, text
+from datetime import datetime
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///database.db')
+connection = engine.connect()
+
+
+@app.route('/')
+def home():
+    query = text('SELECT * FROM logs')
+    result = connection.execute(query).fetchall()
+
+    return render_template('home.html', logs=result)
+
+
+app.run(debug=True, reloader_type='stat', port=5000)
