dac52c474a124eb5a478e617dcd31131f00517cc
Your Namepuclear <you@example.com>
2026-08-19 17:59:59 +1000

    finished the functionality of the log adding system with working connections to the database that display all logs made on the homepage of the website

--- app.py
+++ app.py
@@ -10,17 +10,43 @@ connection = engine.connect()
 
 @app.route('/')
 def home():
-    query = text('SELECT * FROM logs')
+    query = text('SELECT * FROM practice_logs')
     result = connection.execute(query).fetchall()
+
+    print("LOGS FETCHED FROM DATABASE:", result)
 
     return render_template('home.html', logs=result)
 
 
 
-@app.route('/add_log', methods=['GET'])
-def show_form():
+@app.route('/add_log', methods=['GET', 'POST'])
+def add_log():
+    if request.method == 'POST':
+
+        pieces = request.form.get('pieces_practiced')
+        duration = request.form.get('duration_minutes')
+        description = request.form.get('description')
+
+
+        insert_query = text('''
+            INSERT INTO practice_logs (pieces_practiced, duration_minutes, description) 
+            VALUES (:pieces, :duration, :description)
+        ''')
+
+        with engine.begin() as connection:  
+            connection.execute(insert_query, {
+                'pieces': pieces,
+                'duration': duration,
+                'description': description
+            })
+
+       
+        return redirect(url_for('home'))
+
+    
     return render_template('add_log.html')
+    
 
 
-app.run(debug=True, reloader_type='stat', port=5000)
+app.run(debug=True, reloader_type='stat', port=5001)
 
